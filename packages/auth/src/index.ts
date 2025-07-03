b1bf44bbdd7ffa7b1104435d32dd4a0714ae5883
andersonkaiti <anderkaiti@gmail.com>
2025-07-03 17:29:41 -0300

    :sparkles: feat(auth): update ability definitions and permissions to allow only for the allowed Subjects

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,24 +2,19 @@ import {
   AbilityBuilder,
   type CreateAbility,
   createMongoAbility,
-  type ForcedSubject,
   type MongoAbility,
 } from '@casl/ability'
 import type { User } from './models/user'
 import { permissions } from './permissions'
+import type { ProjectSubject } from './subjects/project'
+import type { UserSubject } from './subjects/user'
 
 // action: 'manage' means to manage all permissions
-const actions = ['manage', 'invite', 'delete'] as const
+// const actions = ['manage', 'create', 'invite', 'delete'] as const
 // entity: 'all' means permission on all entities
-const subjects = ['User', 'all'] as const
+// const subjects = ['User', 'Project', 'all'] as const
 
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
