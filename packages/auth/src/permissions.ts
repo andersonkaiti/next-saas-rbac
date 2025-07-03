442090b35ecb0a500930618e7b8a201763252986
andersonkaiti <anderkaiti@gmail.com>
2025-07-03 17:19:12 -0300

    :sparkles: feat(auth): implement role-based permissions and ability definition for users

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,20 @@
+import type { AbilityBuilder } from '@casl/ability'
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+
+type Role = 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void
+
+// by default, CASL removes all permissions, so there is no need to configure 'cannot'.
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN(_, { can }) {
+    can('manage', 'all')
+  },
+  MEMBER(_, { can }) {
+    can('invite', 'User')
+  },
+}
