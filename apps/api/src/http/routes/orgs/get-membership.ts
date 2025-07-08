dc6908cd3141e9e1f243ea5d4626e41fd7c8ca54
andersonkaiti <anderkaiti@gmail.com>
2025-07-07 22:14:32 -0300

    :sparkles: feat(orgs): add getMembership route to retrieve user membership details for an organization

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,47 @@
+import { auth } from '@/http/middlewares/auth'
+import { Role } from '@prisma/client'
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organization/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get user membership on organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.uuid(),
+                role: z.nativeEnum(Role),
+                organizationId: z.uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: roleSchema.parse(membership.role),
+            organizationId: membership.organizationId,
+          },
+        }
+      }
+    )
+}
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -17,6 +17,8 @@ export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
 
+export * from './roles'
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
