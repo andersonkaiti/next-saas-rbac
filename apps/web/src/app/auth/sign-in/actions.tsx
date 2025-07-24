47b011d4a8e6be81cf01df4a519cdf312e9c6741
andersonkaiti <anderkaiti@gmail.com>
2025-07-24 10:50:30 -0300

    :sparkles: feat(signin): implement sign-in form with email and password authentication

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,17 @@
+'use server'
+
+import { signInWithPassword } from '@http/sign-in-with-password'
+
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData
+) {
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: String(email),
+    password: String(password),
+  })
+
+  return 'Success'
+}
