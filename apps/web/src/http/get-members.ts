5bc51cdb45a938a434a925be4fc21567ae926e39
andersonkaiti <anderkaiti@gmail.com>
2025-07-27 17:52:21 -0300

    :sparkles: feat(members): add members page with invites and member list components

---
+++ apps/web/src/http/get-members.ts
@@ -0,0 +1,19 @@
+import type { Role } from '@saas/auth'
+import { api } from './api-client'
+
+interface IGetMembersResponse {
+  members: {
+    id: string
+    userId: string
+    role: Role
+    name: string | null
+    email: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getMembers(org: string) {
+  return await api
+    .get(`organizations/${org}/members`)
+    .json<IGetMembersResponse>()
+}
