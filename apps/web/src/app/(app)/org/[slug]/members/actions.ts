7c29589c20261439b5b00ba2c6c23fb05e2b56cf
andersonkaiti <anderkaiti@gmail.com>
2025-07-28 08:10:56 -0300

    :sparkles: feat(invites): implement create invite functionality with form handling and validation

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,11 +1,80 @@
 'use server'
 
 import { getCurrentOrg } from '@auth/auth'
+import { createInvite } from '@http/create-invite'
 import { removeMember } from '@http/remove-member'
 import { revokeInvite } from '@http/revoke-invite'
 import { updateMember } from '@http/update-member'
-import type { Role } from '@saas/auth'
+import { roleSchema, type Role } from '@saas/auth'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import z from 'zod'
+
+const inviteSchema = z.object({
+  email: z.string().email({ message: 'Invalid e-mail address.' }),
+  role: roleSchema,
+})
+
+export interface IActionState {
+  success: boolean
+  message: string | null
+  errors: z.inferFlattenedErrors<typeof inviteSchema>['fieldErrors'] | null
+  payload: FormData | null
+}
+
+export async function createInviteAction(_: unknown, data: FormData) {
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return {
+      success: false,
+      message: null,
+      errors,
+      payload: data,
+    }
+  }
+
+  const { email, role } = result.data
+
+  const currentOrg = (await getCurrentOrg()) as string
+
+  try {
+    await createInvite({
+      org: currentOrg,
+      email,
+      role,
+    })
+
+    revalidateTag(`${currentOrg}/invites`)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return {
+        success: false,
+        message,
+        errors: null,
+        payload: data,
+      }
+    }
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+      payload: data,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully created the invite.',
+    errors: null,
+    payload: null,
+  }
+}
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
---
+++ apps/web/src/app/(app)/org/[slug]/members/create-invite-form.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { Alert, AlertDescription, AlertTitle } from '@components/ui/alert'
+import { Button } from '@components/ui/button'
+import { Input } from '@components/ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@components/ui/select'
+import { AlertTriangle, Loader2, UserPlus } from 'lucide-react'
+import { useActionState } from 'react'
+import { createInviteAction, type IActionState } from './actions'
+
+export function CreateInviteForm() {
+  const [{ success, message, errors, payload }, formAction, isPending] =
+    useActionState<IActionState, FormData>(createInviteAction, {
+      success: false,
+      message: null,
+      errors: null,
+      payload: null,
+    })
+
+  return (
+    <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Invite failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      {success === true && message && (
+        <Alert variant="success">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Success!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <div className="flex items-start gap-2">
+        <div className="flex-1 space-y-2">
+          <Input
+            name="email"
+            id="email"
+            type="email"
+            placeholder="john@example.com"
+            defaultValue={payload?.get('email') as string}
+          />
+
+          {errors?.email && (
+            <p className="text-xs font-medium text-red-500 dark:text-red-400">
+              {errors.email[0]}
+            </p>
+          )}
+        </div>
+
+        <Select name="role" defaultValue="MEMBER">
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="ADMIN">Admin</SelectItem>
+            <SelectItem value="MEMBER">Member</SelectItem>
+            <SelectItem value="BILLING">Billing</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Button type="submit" className="w-fit" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            <>
+              <UserPlus className="mr-2 size-4" />
+              Invite user
+            </>
+          )}
+        </Button>
+      </div>
+    </form>
+  )
+}
